a0ea5788a155884f1923cb8937df22d3046e4db6
MagmaSKV <82288997+MagmaSKV@users.noreply.github.com>
2025-09-05 14:52:13 +0200

    Update calibrate.c

--- arch/x86/platform/ps4/calibrate.c
+++ arch/x86/platform/ps4/calibrate.c
@@ -49,8 +49,7 @@ static __init unsigned long ps4_measure_tsc_freq(void)
 {
     unsigned long ret;
 
-    // Ignoramos Aeolia y devolvemos valor fijo
-    ret = 2200000000UL; // 2.7 GHz
+    ret = 2500000000UL; // GHz
 
     pr_info("ps4: Forzando TSC frequency a %ld Hz\n", ret);
 
