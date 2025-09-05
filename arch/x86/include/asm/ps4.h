3bc5db4b7fd7a48b97706ca63543404919948613
MagmaSKV <82288997+MagmaSKV@users.noreply.github.com>
2025-09-05 14:53:05 +0200

    Update ps4.h

--- arch/x86/include/asm/ps4.h
+++ arch/x86/include/asm/ps4.h
@@ -13,7 +13,7 @@
 
 #include <linux/irqdomain.h>
 
-#define PS4_DEFAULT_TSC_FREQ 2200000000
+#define PS4_DEFAULT_TSC_FREQ 1594000000
 
 #define EMC_TIMER_BASE 0xd0281000
 #define EMC_TIMER_VALUE 0x28
