65a856f6a529493771ef1fe0f53dc3ba060a3660
MagmaSKV <82288997+MagmaSKV@users.noreply.github.com>
2025-09-28 01:22:38 +0200

    Update calibrate.c

--- arch/x86/platform/ps4/calibrate.c
+++ arch/x86/platform/ps4/calibrate.c
@@ -92,7 +92,7 @@ static int __init ps4_tsc_freq_setup(char *str)
 }
 early_param("ps4_tsc_freq", ps4_tsc_freq_setup);
 
-unsigned long __init ps4_calibrate_tsc(void)
+static unsigned long ps4_calibrate_tsc(void)
 {
     unsigned long tsc_freq;
 
